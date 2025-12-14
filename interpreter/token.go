f183beb1a074d63f117b1d5861ad5fcdbf472e3d
ElshadHu <elsadhumbetli079@gmail.com>
2025-12-14 18:11:18 -0500

    Update token  for new types

--- interpreter/token.go
+++ interpreter/token.go
@@ -1,48 +1,72 @@
 package interpreter
 
-// token.go  - Token definitions
-// we only handle the tokens we need right now
-
+// Token definitions for WTFScript
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string // the actual text
 	Line    int    // line number for error messages
-	Column  int    // Column number for error messages
+	Column  int    // column number for error messages
 }
 
 const (
 	// Special tokens
 	ILLEGAL TokenType = "ILLEGAL"
 	EOF     TokenType = "EOF"
+	COMMENT TokenType = "COMMENT"
 
 	// Identifiers and literals
-	IDENT TokenType = "IDENT"
-	INT   TokenType = "INT"
+	IDENT  TokenType = "IDENT"
+	INT    TokenType = "INT"
+	FLOAT  TokenType = "FLOAT"
+	STRING TokenType = "STRING"
+	TRUE   TokenType = "TRUE"
+	FALSE  TokenType = "FALSE"
 
 	// Operators
-	ASSIGN TokenType = "="
-	PLUS   TokenType = "+"
-	MINUS  TokenType = "-"
+	ASSIGN   TokenType = "="
+	PLUS     TokenType = "+"
+	MINUS    TokenType = "-"
+	ASTERISK TokenType = "*"
+	SLASH    TokenType = "/"
 
 	// Delimiters
 	SEMICOLON TokenType = ";"
+	COMMA     TokenType = ","
 	LPAREN    TokenType = "("
 	RPAREN    TokenType = ")"
 
-	// Keywords  with one type now
-	TYPE_INT TokenType = "INT_TYPE"
+	// Type Keywords
+	TYPE_INT      TokenType = "INT_TYPE"
+	TYPE_UINT     TokenType = "UINT_TYPE"
+	TYPE_FLOAT    TokenType = "FLOAT_TYPE"
+	TYPE_UNOFLOAT TokenType = "UNOFLOAT_TYPE"
+	TYPE_BOOL     TokenType = "BOOL_TYPE"
+	TYPE_STRING   TokenType = "STRING_TYPE"
 )
 
 // keywords maps keyword strings to their TokenType
 var keywords = map[string]TokenType{
-	"int": TYPE_INT,
+	"int":      TYPE_INT,
+	"uint":     TYPE_UINT,
+	"float":    TYPE_FLOAT,
+	"unofloat": TYPE_UNOFLOAT,
+	"bool":     TYPE_BOOL,
+	"string":   TYPE_STRING,
+	"true":     TRUE,
+	"false":    FALSE,
 }
 
+// LookupIdent checks if an identifier is a keyword
 func LookupIdent(ident string) TokenType {
-	if token, ok := keywords[ident]; ok {
-		return token
+	if tok, ok := keywords[ident]; ok {
+		return tok
 	}
 	return IDENT
+}
+
+// String returns a human-readable representation of the token
+func (t Token) String() string {
+	return string(t.Type) + ":" + t.Literal
 }
